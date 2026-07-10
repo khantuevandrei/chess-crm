858b6127f384197a34c513b4b9ae84788e61465b
Khantuev Andrei <khantuevandrei@gmail.com>
2026-07-10 17:16:52 +0500

    fix: add error messages for each field in Trainers when updating

--- resources/js/Pages/Trainers/Index.vue
+++ resources/js/Pages/Trainers/Index.vue
@@ -72,23 +72,31 @@ function deleteTrainer(id) {
     })">
         <input v-model="form.first_name" placeholder="First name" />
         <p v-if="form.errors.first_name" style="color:red">{{ form.errors.first_name }}</p>
+
         <input v-model="form.last_name" placeholder="Last name" />
         <p v-if="form.errors.last_name" style="color:red">{{ form.errors.last_name }}</p>
+
         <input v-model="form.email" placeholder="Email" />
         <p v-if="form.errors.email" style="color:red">{{ form.errors.email }}</p>
+
         <input v-model="form.phone" placeholder="Phone" />
         <p v-if="form.errors.phone" style="color:red">{{ form.errors.phone }}</p>
+
         <input v-model="form.specialization" placeholder="Specialization" />
         <p v-if="form.errors.specialization" style="color:red">{{ form.errors.specialization }}</p>
+
         <input v-model="form.rating" placeholder="Rating" />
         <p v-if="form.errors.rating" style="color:red">{{ form.errors.rating }}</p>
+
         <select v-model="form.branch_id">
             <option v-for="branch in branches" :key="branch.id" :value="branch.id">
                 {{ branch.name }}
             </option>
         </select>
+
         <input v-model="form.bio" placeholder="Bio" />
         <p v-if="form.errors.bio" style="color:red">{{ form.errors.bio }}</p>
+
         <button type="submit" :disabled="form.processing">Add</button>
     </form>
 
@@ -96,17 +104,32 @@ function deleteTrainer(id) {
         <li v-for="trainer in trainers" :key="trainer.id">
             <template v-if="editingId === trainer.id">
                 <input v-model="editForm.first_name" />
+                <p v-if="editForm.errors.first_name" style="color:red">{{ editForm.errors.first_name }}</p>
+
                 <input v-model="editForm.last_name" />
+                <p v-if="editForm.errors.last_name" style="color:red">{{ editForm.errors.last_name }}</p>
+
                 <input v-model="editForm.email" />
+                <p v-if="editForm.errors.email" style="color:red">{{ editForm.errors.email }}</p>
+
                 <input v-model="editForm.phone" />
+                <p v-if="editForm.errors.phone" style="color:red">{{ editForm.errors.phone }}</p>
+
                 <input v-model="editForm.specialization" />
+                <p v-if="editForm.errors.specialization" style="color:red">{{ editForm.errors.specialization }}</p>
+
                 <input v-model="editForm.rating" />
+                <p v-if="editForm.errors.rating" style="color:red">{{ editForm.errors.rating }}</p>
+
                 <select v-model="editForm.branch_id">
                     <option v-for="branch in branches" :key="branch.id" :value="branch.id">
                         {{ branch.name }}
                     </option>
                 </select>
+
                 <input v-model="editForm.bio" />
+                <p v-if="editForm.errors.bio" style="color:red">{{ editForm.errors.bio }}</p>
+
                 <button @click="updateTrainer(trainer.id)">Save</button>
                 <button @click="cancelEdit()">Cancel</button>
             </template>
